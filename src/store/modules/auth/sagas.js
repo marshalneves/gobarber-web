6639b659a88430b79905aa80ec5e042b9d975b10
Marshal Neves <mcneves75@gmail.com>
2020-02-26 20:49:28 -0300

    Implementing Sign Up

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, all, call, put } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import history from '~/services/history';
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
@@ -14,15 +15,40 @@ export function* signIn({ payload }) {
 
     const { token, user } = response.data;
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('User is not a provider!');
       return;
     }
 
     yield put(signInSuccess(token, user));
+    toast.success('Account has been created with success.');
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Auth Fails!');
     yield put(signFailure());
   }
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (error) {
+    console.tron.log(error);
+    toast.error('Fails on save data!');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
