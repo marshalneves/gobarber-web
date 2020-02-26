12b7c815b687f6e9b177b7ec3f0570733f3a7817
Marshal Neves <mcneves75@gmail.com>
2020-02-26 16:47:42 -0300

    Implementing Authentication

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
-import createStoreSagaMiddleware from 'redux-saga';
+import createSagaMiddleware from 'redux-saga';
 import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
 
-const sagaMonitor =
-  process.env.NODE_ENV === 'development'
-    ? console.tron.createSagaMonitor()
-    : null;
+const sagaMonitor = console.tron.createSagaMonitor();
+// process.env.NODE_ENV === 'development'
+//   ? console.tron.createSagaMonitor()
+//   : null;
 
-const sagaMiddleware = createStoreSagaMiddleware({ sagaMonitor });
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
